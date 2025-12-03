f3ab6ea34a434426bc4bbea17cd833bae89c99a7
minbros <pristo24631@gmail.com>
2025-12-03 13:59:55 +0900

    Initial commit

--- discovery/src/main/java/store/_0982/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/store/_0982/discovery/DiscoveryApplication.java
@@ -2,7 +2,9 @@ package store._0982.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryApplication {
 
