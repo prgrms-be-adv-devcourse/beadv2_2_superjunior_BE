f34f54c2ecf6d4a487341392d71b7f41927716f1
minbros <pristo24631@gmail.com>
2025-12-03 20:39:48 +0900

    feat: DDL 스크립트 추가

---
+++ docs/script/sql/point-schema.sql
@@ -0,0 +1,92 @@
+create schema point_schema;
+
+create table point_schema.payment_point
+(
+    payment_point_id uuid                                   not null
+        constraint payment_point_pk
+            primary key,
+    member_id        uuid                                   not null,
+    pg_order_id      uuid                                   not null
+        constraint payment_point_pk_2
+            unique,
+    payment_method   varchar(30)                            not null,
+    payment_key      varchar(50)                            not null,
+    amount           integer                                not null,
+    status           varchar(20)                            not null
+        constraint status_check
+            check ((status)::text = ANY
+                   (ARRAY [('REQUESTED'::character varying)::text, ('COMPLETED'::character varying)::text, ('FAILED'::character varying)::text])),
+    fail_message     text,
+    created_at       timestamp with time zone default now() not null,
+    requested_at     timestamp with time zone,
+    approved_at      timestamp with time zone,
+    updated_at       timestamp with time zone
+);
+
+comment on table point_schema.payment_point is '결제';
+
+comment on column point_schema.payment_point.payment_point_id is '결제 ID';
+
+comment on column point_schema.payment_point.member_id is '멤버 ID';
+
+comment on column point_schema.payment_point.pg_order_id is 'PG사 주문 ID';
+
+comment on column point_schema.payment_point.payment_method is '결제 방식';
+
+comment on column point_schema.payment_point.payment_key is 'PG사 결제 키';
+
+comment on column point_schema.payment_point.amount is '금액';
+
+comment on column point_schema.payment_point.status is '상태';
+
+comment on column point_schema.payment_point.fail_message is '실패 메시지';
+
+comment on column point_schema.payment_point.created_at is '생성 시간';
+
+comment on column point_schema.payment_point.requested_at is '결제 요청 시간';
+
+comment on column point_schema.payment_point.approved_at is '결제 승인 시간';
+
+comment on column point_schema.payment_point.updated_at is '수정 시간';
+
+alter table point_schema.payment_point
+    owner to postgres;
+
+create table point_schema.payment_point_failure
+(
+    failure_id       uuid                                   not null
+        constraint payment_point_failure_pk
+            primary key,
+    payment_point_id uuid                                   not null
+        constraint payment_point_failure_pk_2
+            unique
+        constraint payment_point_failure_payment_point_payment_point_id_fk
+            references point_schema.payment_point,
+    payment_key      varchar(50)                            not null,
+    error_code       varchar(30),
+    error_message    text,
+    amount           integer,
+    raw_payload      text                                   not null,
+    created_at       timestamp with time zone default now() not null
+);
+
+comment on table point_schema.payment_point_failure is '결제 실패';
+
+comment on column point_schema.payment_point_failure.failure_id is '결제 실패 ID';
+
+comment on column point_schema.payment_point_failure.payment_point_id is '결제 ID';
+
+comment on column point_schema.payment_point_failure.payment_key is 'PG사 결제 키';
+
+comment on column point_schema.payment_point_failure.error_code is '에러 코드';
+
+comment on column point_schema.payment_point_failure.error_message is '에러 메시지';
+
+comment on column point_schema.payment_point_failure.amount is '금액';
+
+comment on column point_schema.payment_point_failure.raw_payload is 'PG사 응답 JSON 데이터';
+
+comment on column point_schema.payment_point_failure.created_at is '생성 시간';
+
+alter table point_schema.payment_point_failure
+    owner to postgres;
---
+++ docs/script/sql/product-schema.sql
@@ -0,0 +1,129 @@
+create schema product_schema;
+
+create table product_schema.product
+(
+    product_id   uuid                                   not null
+        constraint product_pk
+            primary key,
+    name         varchar(100)                           not null,
+    price        integer                                not null,
+    category     varchar(20)                            not null
+        constraint category_check
+            check ((category)::text = ANY
+                   (ARRAY [('HOME'::character varying)::text, ('FOOD'::character varying)::text, ('HEALTH'::character varying)::text, ('BEAUTY'::character varying)::text, ('FASHION'::character varying)::text, ('ELECTRONICS'::character varying)::text, ('KIDS'::character varying)::text, ('HOBBY'::character varying)::text, ('PET'::character varying)::text])),
+    description  text                                   not null,
+    stock        integer                  default 0     not null,
+    original_url varchar(255)                           not null,
+    seller_id    uuid                                   not null,
+    created_at   timestamp with time zone default now() not null,
+    updated_at   timestamp with time zone
+);
+
+comment on table product_schema.product is '상품';
+
+comment on column product_schema.product.product_id is '상품 ID';
+
+comment on column product_schema.product.name is '상품 이름';
+
+comment on column product_schema.product.price is '상품 가격';
+
+comment on column product_schema.product.category is '상품 카테고리';
+
+comment on column product_schema.product.description is '상품 설명';
+
+comment on column product_schema.product.stock is '재고';
+
+comment on column product_schema.product.original_url is '원상품 링크';
+
+comment on column product_schema.product.seller_id is '판매자 ID';
+
+comment on column product_schema.product.created_at is '등록일';
+
+comment on column product_schema.product.updated_at is '수정일';
+
+alter table product_schema.product
+    owner to postgres;
+
+create table product_schema.group_purchase
+(
+    group_purchase_id uuid                                                            not null
+        constraint group_purchase_pk
+            primary key,
+    min_quantity      integer                  default 1                              not null,
+    max_quantity      integer,
+    title             varchar(100)                                                    not null,
+    description       text,
+    discounted_price  integer                  default 0                              not null,
+    status            varchar(20)              default 'SCHEDULED'::character varying not null
+        constraint status_check
+            check ((status)::text = ANY
+                   (ARRAY [('SCHEDULED'::character varying)::text, ('OPEN'::character varying)::text, ('SUCCESS'::character varying)::text, ('FAILED'::character varying)::text])),
+    start_date        timestamp with time zone                                        not null,
+    end_date          timestamp with time zone                                        not null,
+    seller_id         uuid                                                            not null,
+    product_id        uuid                                                            not null
+        constraint group_purchase_product_product_id_fk
+            references product_schema.product,
+    created_at        timestamp with time zone default now()                          not null,
+    updated_at        timestamp with time zone
+);
+
+comment on table product_schema.group_purchase is '공동 구매';
+
+comment on column product_schema.group_purchase.group_purchase_id is '공동 구매 ID';
+
+comment on column product_schema.group_purchase.min_quantity is '공구 성공 최소 수량';
+
+comment on column product_schema.group_purchase.max_quantity is '공구 참여 최대 수량';
+
+comment on column product_schema.group_purchase.title is '공구 제목';
+
+comment on column product_schema.group_purchase.description is '공구 상세 설명';
+
+comment on column product_schema.group_purchase.discounted_price is '공구 할인 가격';
+
+comment on column product_schema.group_purchase.status is '공구 상태';
+
+comment on column product_schema.group_purchase.start_date is '공구 시작 시간';
+
+comment on column product_schema.group_purchase.end_date is '공구 종료 시간';
+
+comment on column product_schema.group_purchase.seller_id is '판매자 ID';
+
+comment on column product_schema.group_purchase.product_id is '상품 ID';
+
+comment on column product_schema.group_purchase.created_at is '등록일';
+
+comment on column product_schema.group_purchase.updated_at is '수정일';
+
+alter table product_schema.group_purchase
+    owner to postgres;
+
+create table product_schema.image
+(
+    image_id    uuid                                   not null
+        constraint image_pk
+            primary key,
+    file_name   varchar(255)                           not null,
+    file_type   varchar(10)                            not null,
+    size        bigint                                 not null,
+    file_path   varchar(255)                           not null,
+    uploaded_at timestamp with time zone default now() not null
+);
+
+comment on table product_schema.image is '상품 이미지';
+
+comment on column product_schema.image.image_id is '이미지 ID';
+
+comment on column product_schema.image.file_name is '파일 이름';
+
+comment on column product_schema.image.file_type is '파일 타입';
+
+comment on column product_schema.image.size is '파일 크기';
+
+comment on column product_schema.image.file_path is '파일 경로';
+
+comment on column product_schema.image.uploaded_at is '업로드일';
+
+alter table product_schema.image
+    owner to postgres;
