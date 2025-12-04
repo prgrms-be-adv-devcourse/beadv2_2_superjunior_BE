064454034b9b0b6d18b6656c5d2d317b8d0fc8fc
Huiyeongkim <huiyeong9619@naver.com>
2025-12-04 10:00:16 +0900

    refactor: CustomErrorCode에 기본 메시지 추가

--- product/src/main/java/store/_0982/product/common/exception/CustomErrorCode.java
+++ product/src/main/java/store/_0982/product/common/exception/CustomErrorCode.java
@@ -9,25 +9,14 @@ import org.springframework.http.HttpStatus;
 public enum CustomErrorCode {
 
     // 400 Bad Request
-    BAD_REQUEST(HttpStatus.BAD_REQUEST),
-
-    // 401 Unauthorized
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED),
-
-    // 403 Forbidden
-    FORBIDDEN(HttpStatus.FORBIDDEN),
-
-    // 404 Not Found
-    NOT_FOUND(HttpStatus.NOT_FOUND),
-
-    // 409 Conflict
-    CONFLICT(HttpStatus.CONFLICT),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
 
     // 500 Internal Server Error
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다."),
 
     // 503 Service Unavailable
-    SERVICE_UNAVAILABLE(HttpStatus.SERVICE_UNAVAILABLE);
+    SERVICE_UNAVAILABLE(HttpStatus.SERVICE_UNAVAILABLE, "서비스를 사용할 수 없습니다.");
 
     private final HttpStatus httpStatus;
+    private final String message;
 }
