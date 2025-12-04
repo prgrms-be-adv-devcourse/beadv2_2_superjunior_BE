89ef022c9a4cd1a84b3b1ae47e42e6dd26de5dc7
Huiyeongkim <huiyeong9619@naver.com>
2025-12-04 12:33:15 +0900

    feat: group purchase 도메인 생성

--- product/src/main/java/store/_0982/product/domain/GroupPurchase.java
+++ product/src/main/java/store/_0982/product/domain/GroupPurchase.java
@@ -1,17 +1,84 @@
 package store._0982.product.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "\"group_purchase\"")
 public class GroupPurchase {
     @Id
+    private UUID groupPurchaseId;
+
+    @Column(name = "min_quantity", nullable = false)
+    private int minQuantity;
+
+    @Column(name = "max_quantity")
+    private int maxQuantity;
+
+    @Column(name = "title", nullable = false, length = 100)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "discounted_price", nullable = false)
+    private int discountedPrice;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private GroupPurchaseStatus status;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDate endDate;
+
+    @Column(name = "seller_id", nullable = false)
+    private UUID sellerId;
+
+    @Column(name = "product_id", nullable = false)
     private UUID productId;
+
+    @Column(name = "created_at", nullable = false)
+    @CreationTimestamp
+    private OffsetDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private OffsetDateTime updatedAt;
+    
+    public GroupPurchase(int mintQuantity,
+                         int maxQuantity,
+                         String title,
+                         String description,
+                         int discountedPrice,
+                         LocalDateTime startDate,
+                         LocalDate endDate,
+                         UUID sellerId,
+                         UUID productId){
+        this.groupPurchaseId = UUID.randomUUID();
+        this.minQuantity = mintQuantity;
+        this.maxQuantity = maxQuantity;
+        this.title = title;
+        this.description = description;
+        this.discountedPrice = discountedPrice;
+        this.status = GroupPurchaseStatus.SCHEDULED;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.sellerId = sellerId;
+        this.productId = productId;
+    }
 
 }
