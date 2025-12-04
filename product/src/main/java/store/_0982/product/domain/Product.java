2b24cc8c5a34bfdf404287368f5f2e1f27486650
Huiyeongkim <huiyeong9619@naver.com>
2025-12-04 11:40:14 +0900

    feat: product domain 생성

--- product/src/main/java/store/_0982/product/domain/Product.java
+++ product/src/main/java/store/_0982/product/domain/Product.java
@@ -1,18 +1,68 @@
 package store._0982.product.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "\"product\"")
 public class Product {
     @Id
     private UUID productId;
 
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
 
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @Column(name = "category", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ProductCategory category;
+
+    @Column(name = "description", nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "stock", nullable = false)
+    private int stock;
+
+    @Column(name = "original_url")
+    private String originalUrl;
+
+    @Column(name = "seller_id", nullable = false)
+    private UUID sellerId;
+
+    @Column(name = "created_at", nullable = false)
+    @CreationTimestamp
+    private OffsetDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private OffsetDateTime updatedAt;
+
+    public Product(String name,
+                   int price,
+                   ProductCategory category,
+                   String description,
+                   int stock,
+                   String originalUrl,
+                   UUID sellerId) {
+
+        this.productId = UUID.randomUUID();
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.description = description;
+        this.stock  = stock;
+        this.originalUrl = originalUrl;
+        this.sellerId = sellerId;
+    }
 }
