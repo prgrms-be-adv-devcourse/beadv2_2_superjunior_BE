e610b249e7c23668ab621880a2e88010ea9e1a48
Huiyeongkim <huiyeong9619@naver.com>
2025-12-09 12:23:21 +0900

    feat: seller balance domain 생성

---
+++ order/src/main/java/store/_0982/order/domain/settlement/SellerBalance.java
@@ -0,0 +1,40 @@
+package store._0982.order.domain.settlement;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "seller_balance", schema = "order_schema")
+public class SellerBalance {
+
+    @Id
+    @Column(name = "balance_id", nullable = false)
+    private UUID balanceId;
+
+    @Column(name = "member_id", nullable = false, unique = true)
+    private UUID memberId;
+
+    @Column(name = "settlement_balance", nullable = false)
+    private Long settlementBalance;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private OffsetDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private OffsetDateTime updatedAt;
+
+}
