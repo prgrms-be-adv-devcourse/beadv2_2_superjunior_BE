d1f64e3eee31fd3f4e2d19c1a2964723a5dc0cff
minbros <pristo24631@gmail.com>
2025-12-09 17:28:44 +0900

    feat: 기본 API 구현

---
+++ notification/src/main/java/store/_0982/notification/infrastructure/NotificationAdapter.java
@@ -0,0 +1,38 @@
+package store._0982.notification.infrastructure;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+import store._0982.notification.domain.Notification;
+import store._0982.notification.domain.NotificationRepository;
+import store._0982.notification.domain.NotificationStatus;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+@RequiredArgsConstructor
+public class NotificationAdapter implements NotificationRepository {
+    private final NotificationJpaRepository notificationJpaRepository;
+
+    @Override
+    public Optional<Notification> findById(UUID uuid) {
+        return notificationJpaRepository.findById(uuid);
+    }
+
+    @Override
+    public List<Notification> findByMemberIdAndStatus(UUID memberId, NotificationStatus status) {
+        return notificationJpaRepository.findByMemberIdAndStatus(memberId, status);
+    }
+
+    public Page<Notification> findByMemberId(UUID memberId, Pageable pageable) {
+        return notificationJpaRepository.findByMemberId(memberId, pageable);
+    }
+
+    @Override
+    public Page<Notification> findByMemberIdAndStatus(UUID memberId, NotificationStatus status, Pageable pageable) {
+        return notificationJpaRepository.findByMemberIdAndStatus(memberId, status, pageable);
+    }
+}
