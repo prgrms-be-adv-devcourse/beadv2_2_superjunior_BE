5fc85bc0953f2eb7be3d244daba9119273bf317d
minbros <pristo24631@gmail.com>
2025-12-09 12:33:53 +0900

    feat: Autoconfiguration을 이용해 각 모듈에 자동적으로 로깅이 적용되게 설정

--- common/src/main/java/store/_0982/common/config/LoggingAutoConfig.java
+++ common/src/main/java/store/_0982/common/config/LoggingAutoConfig.java
@@ -1,5 +1,6 @@
 package store._0982.common.config;
 
+import org.aspectj.lang.annotation.Aspect;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -7,9 +8,9 @@ import org.springframework.context.annotation.Bean;
 import store._0982.common.log.LoggingAspect;
 
 @AutoConfiguration
-@ConditionalOnClass(name = "org.aspectj.lang.annotation.Aspect")
+@ConditionalOnClass(Aspect.class)
 @ConditionalOnProperty(
-        prefix = "logging.aspect",
+        prefix = "common.logging",
         name = "enabled",
         havingValue = "true",
         matchIfMissing = true
