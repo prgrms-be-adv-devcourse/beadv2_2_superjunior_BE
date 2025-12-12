f994446da513acba9c3c33032a8bd64ce6693bc9
Huiyeongkim <huiyeong9619@naver.com>
2025-12-12 17:10:51 +0900

    refactor: @RequestBody 전송하기 위해 @PostMapping으로 변경

--- order/src/main/java/store/_0982/order/client/MemberFeignClient.java
+++ order/src/main/java/store/_0982/order/client/MemberFeignClient.java
@@ -1,7 +1,7 @@
 package store._0982.order.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import store._0982.common.dto.ResponseDto;
 import store._0982.order.client.dto.SellerAccountInfo;
@@ -12,7 +12,7 @@ import java.util.List;
 @FeignClient(name = "member-service", url = "${gateway.host}")
 public interface MemberFeignClient {
 
-    @GetMapping("/internal/members/seller-account")
+    @PostMapping("/internal/members/seller-account")
     ResponseDto<List<SellerAccountInfo>> getSellerAccountInfos(@RequestBody SellerAccountListRequest request);
 
 }
