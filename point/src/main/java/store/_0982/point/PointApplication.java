ceaf9aff410a25a7e29282a96a99401bbc1102f6
minbros <pristo24631@gmail.com>
2025-12-12 10:52:46 +0900

    feat: 내부용 API 추가

--- point/src/main/java/store/_0982/point/PointApplication.java
+++ point/src/main/java/store/_0982/point/PointApplication.java
@@ -2,8 +2,10 @@ package store._0982.point;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PointApplication {
 
     public static void main(String[] args) {
--- point/src/main/java/store/_0982/point/presentation/InternalPointController.java
+++ point/src/main/java/store/_0982/point/presentation/InternalPointController.java
@@ -1,6 +1,6 @@
 package store._0982.point.presentation;
 
-import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,39 +15,30 @@ import store._0982.point.presentation.dto.PointReturnRequest;
 
 import java.util.UUID;
 
+@Hidden
 @RestController
-@RequestMapping("/api/points")
+@RequestMapping("/internal/points")
 @RequiredArgsConstructor
-public class MemberPointController {
+public class InternalPointController {
     private final MemberPointService memberPointService;
 
-    @Operation(summary = "포인트 조회", description = "선택한 멤버의 포인트를 조회한다.")
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping
-    public ResponseDto<MemberPointInfo> getPoints(@RequestHeader(HeaderName.ID) UUID memberId) {
-        MemberPointInfo info = memberPointService.getPoints(memberId);
-        return new ResponseDto<>(HttpStatus.OK, info, "포인트 조회 성공");
-    }
-
-    @Operation(summary = "포인트 차감", description = "선택한 멤버의 포인트를 차감한다.")
     @ResponseStatus(HttpStatus.CREATED)
     @ControllerLog
     @PostMapping("/deduct")
     public ResponseDto<MemberPointInfo> deductPoints(
-            @RequestHeader(HeaderName.ID) UUID memberId,
-            @RequestBody @Valid PointDeductRequest request
+            @Valid @RequestBody PointDeductRequest request,
+            @RequestHeader(HeaderName.ID) UUID memberId
     ) {
         MemberPointInfo info = memberPointService.deductPoints(memberId, request.toCommand());
         return new ResponseDto<>(HttpStatus.CREATED, info, "포인트 차감 완료");
     }
 
-    @Operation(summary = "포인트 반환", description = "선택한 멤버의 포인트를 반환한다.")
     @ResponseStatus(HttpStatus.CREATED)
     @ControllerLog
     @PostMapping("/return")
     public ResponseDto<MemberPointInfo> returnPoints(
-            @RequestHeader(HeaderName.ID) UUID memberId,
-            @RequestBody @Valid PointReturnRequest request
+            @Valid @RequestBody PointReturnRequest request,
+            @RequestHeader(HeaderName.ID) UUID memberId
     ) {
         MemberPointInfo info = memberPointService.returnPoints(memberId, request.toCommand());
         return new ResponseDto<>(HttpStatus.CREATED, info, "포인트 반환 완료");
