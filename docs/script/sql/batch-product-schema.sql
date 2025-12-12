429d2e33029e10a16639921f160ef49dd3a1bde5
sumyeom <Qkrwlstjd!0621>
2025-12-12 21:20:13 +0900

    docs: batch-product-schema, batch_product_schema로 수정

--- docs/script/sql/batch-product-schema.sql
+++ docs/script/sql/batch-product-schema.sql
@@ -1,6 +1,6 @@
-create schema batch_schema;
+create schema batch_product_schema;
 
-create table batch_schema.batch_job_instance
+create table batch_product_schema.batch_job_instance
 (
     job_instance_id bigint       not null
         constraint batch_job_instance_pk
@@ -11,10 +11,10 @@ create table batch_schema.batch_job_instance
     constraint job_inst_un unique (job_name, job_key)
 );
 
-alter table batch_schema.batch_job_instance
+alter table batch_product_schema.batch_job_instance
     owner to postgres;
 
-create table batch_schema.batch_job_execution
+create table batch_product_schema.batch_job_execution
 (
     job_execution_id bigint    not null
         constraint batch_job_execution_pk
@@ -32,10 +32,10 @@ create table batch_schema.batch_job_execution
     last_updated     timestamp
 );
 
-alter table batch_schema.batch_job_execution
+alter table batch_product_schema.batch_job_execution
     owner to postgres;
 
-create table batch_schema.batch_job_execution_params
+create table batch_product_schema.batch_job_execution_params
 (
     job_execution_id bigint       not null
         constraint job_exec_params_fk
@@ -46,10 +46,10 @@ create table batch_schema.batch_job_execution_params
     identifying      char(1)      not null
 );
 
-alter table batch_schema.batch_job_execution_params
+alter table batch_product_schema.batch_job_execution_params
     owner to postgres;
 
-create table batch_schema.batch_step_execution
+create table batch_product_schema.batch_step_execution
 (
     step_execution_id  bigint       not null
         constraint batch_step_execution_pk
@@ -76,7 +76,7 @@ create table batch_schema.batch_step_execution
     last_updated       timestamp
 );
 
-alter table batch_schema.batch_step_execution
+alter table batch_product_schema.batch_step_execution
     owner to postgres;
 
 create table batch_schema.batch_step_execution_context
@@ -90,10 +90,10 @@ create table batch_schema.batch_step_execution_context
     serialized_context text
 );
 
-alter table batch_schema.batch_step_execution_context
+alter table batch_product_schema.batch_step_execution_context
     owner to postgres;
 
-create table batch_schema.batch_job_execution_context
+create table batch_product_schema.batch_job_execution_context
 (
     job_execution_id   bigint        not null
         constraint batch_job_execution_context_pk
@@ -104,9 +104,9 @@ create table batch_schema.batch_job_execution_context
     serialized_context text
 );
 
-alter table batch_schema.batch_job_execution_context
+alter table batch_product_schema.batch_job_execution_context
     owner to postgres;
 
-create sequence batch_schema.batch_step_execution_seq maxvalue 9223372036854775807 no cycle;
-create sequence batch_schema.batch_job_execution_seq maxvalue 9223372036854775807 no cycle;
-create sequence batch_schema.batch_job_seq maxvalue 9223372036854775807 no cycle;
+create sequence batch_product_schema.batch_step_execution_seq maxvalue 9223372036854775807 no cycle;
+create sequence batch_product_schema.batch_job_execution_seq maxvalue 9223372036854775807 no cycle;
+create sequence batch_product_schema.batch_job_seq maxvalue 9223372036854775807 no cycle;
