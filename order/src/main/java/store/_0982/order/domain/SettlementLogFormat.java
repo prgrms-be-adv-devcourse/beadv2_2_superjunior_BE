de00ef46b054fbcd410dfc7585880bcb739642e3
Huiyeongkim <huiyeong9619@naver.com>
2025-12-12 13:32:00 +0900

    refactor: DailySettlementService SettlementLogFormat 적용'

--- order/src/main/java/store/_0982/order/domain/SettlementLogFormat.java
+++ order/src/main/java/store/_0982/order/domain/SettlementLogFormat.java
@@ -6,5 +6,9 @@ public class SettlementLogFormat {
     public static final String COMPLETE = "[SCHEDULER] [%s] completed";
     public static final String FAIL = "[SCHEDULER] [%s] failed";
 
+    public static final String DAILY_SETTLEMENT_START = "[DAILY_SETTLEMENT] [Seller:%s] started - %d";
+    public static final String DAILY_SETTLEMENT_COMPLETE = "[DAILY_SETTLEMENT] [Seller:%s] completed";
+    public static final String DAILY_SETTLEMENT_FAIL = "[DAILY_SETTLEMENT] [Seller:%s] failed - %s";
+
     private SettlementLogFormat() {}
 }
