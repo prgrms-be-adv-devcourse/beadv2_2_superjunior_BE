e5f0bd6237eca4b5a173179a7876f405ce0942c1
Huiyeongkim <huiyeong9619@naver.com>
2025-12-12 13:28:19 +0900

    refactor: SettlementLogFormat 사용

---
+++ order/src/main/java/store/_0982/order/domain/SettlementLogFormat.java
@@ -0,0 +1,10 @@
+package store._0982.order.domain;
+
+public class SettlementLogFormat {
+
+    public static final String START = "[SCHEDULER] [%s] started";
+    public static final String COMPLETE = "[SCHEDULER] [%s] completed";
+    public static final String FAIL = "[SCHEDULER] [%s] failed";
+
+    private SettlementLogFormat() {}
+}
