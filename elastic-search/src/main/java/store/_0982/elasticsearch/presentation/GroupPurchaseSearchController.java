8b01a9d064a98f7e86656c38fca8736ad49309b7
ajw <anjy0821@naver.com>
2025-12-16 22:41:08 +0900

    Feat: sellerId 직접 주입 추가

--- elastic-search/src/main/java/store/_0982/elasticsearch/presentation/GroupPurchaseSearchController.java
+++ elastic-search/src/main/java/store/_0982/elasticsearch/presentation/GroupPurchaseSearchController.java
@@ -73,7 +73,7 @@ public class GroupPurchaseSearchController {
         return new ResponseDto<>(HttpStatus.OK, result, "문서 검색 완료.");
     }
 
-    @Operation(summary = "공동구매 문서 검색", description = "키워드(제목, 설명) + 상태 기준으로 공동구매를 검색합니다.")
+    @Operation(summary = "공동구매 문서 검색", description = "키워드(제목, 설명) + 상태 + sellerId 기준으로 공동구매를 검색합니다.")
     @ResponseStatus(HttpStatus.OK)
     @ControllerLog
     @GetMapping("/search/seller")
