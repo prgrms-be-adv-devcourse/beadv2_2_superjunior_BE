c30bccbdfe80368fbd2c1ac10f83458a30c2ce89
Huiyeongkim <huiyeong9619@naver.com>
2025-12-24 09:01:54 +0900

    refactor: batch 패키지 구조 정리

---
+++ batch/src/main/java/store/_0982/batch/application/cart/CartService.java
@@ -0,0 +1,11 @@
+package store._0982.batch.application.cart;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartService {
+
+    public void cleanUpZeroCarts() {
+
+    }
+}
---
+++ batch/src/main/java/store/_0982/batch/batch/grouppurchase/processor/ReturnProcessor.java
@@ -0,0 +1,29 @@
+package store._0982.batch.batch.grouppurchase.processor;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+import store._0982.batch.batch.grouppurchase.dto.GroupPurchaseResult;
+import store._0982.commerce.infrastructure.client.order.OrderClient;
+import store._0982.commerce.domain.grouppurchase.GroupPurchase;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class ReturnProcessor implements ItemProcessor<GroupPurchase, GroupPurchaseResult> {
+
+    private final OrderClient orderClient;
+
+    @Override
+    public GroupPurchaseResult process(GroupPurchase groupPurchase) throws Exception {
+        try{
+            orderClient.returnOrders(groupPurchase.getGroupPurchaseId());
+            log.info("환불 처리 완료: groupPurchaseId={}", groupPurchase.getGroupPurchaseId());
+            return new GroupPurchaseResult(groupPurchase, true);
+        }catch (Exception e){
+            log.error("환불 실패 : groupPurchaseId={}",groupPurchase.getGroupPurchaseId(),e);
+            return new GroupPurchaseResult(groupPurchase, false);
+        }
+    }
+}
