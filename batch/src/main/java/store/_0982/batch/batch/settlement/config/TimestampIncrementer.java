70aa9630c76e0c5662217cb39bc1946399183491
Huiyeongkim <huiyeong9619@naver.com>
2026-01-09 09:08:53 +0900

    refactor: JobParameter 가 null 인 경우 NPE 에러 막기 위해  수정

--- batch/src/main/java/store/_0982/batch/batch/settlement/config/TimestampIncrementer.java
+++ batch/src/main/java/store/_0982/batch/batch/settlement/config/TimestampIncrementer.java
@@ -3,15 +3,22 @@ package store._0982.batch.batch.settlement.config;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersIncrementer;
+import org.springframework.lang.NonNull;
 
 /**
  * 타임스탬프 기반 JobParametersIncrementer
  */
 public class TimestampIncrementer implements JobParametersIncrementer {
 
+    @NonNull
     @Override
     public JobParameters getNext(JobParameters parameters) {
-        return new JobParametersBuilder(parameters)
+        JobParametersBuilder builder =
+                (parameters == null)
+                        ? new JobParametersBuilder()
+                        : new JobParametersBuilder(parameters);
+
+        return builder
                 .addLong("timestamp", System.currentTimeMillis())
                 .toJobParameters();
     }
