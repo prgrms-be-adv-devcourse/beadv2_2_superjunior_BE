04a85ae339e5a434d0d2a566622e1e933a1ffc29
minbros <pristo24631@gmail.com>
2026-01-11 10:50:18 +0900

    refactor: 필요 없는 enum 삭제

--- point/src/main/java/store/_0982/point/domain/constant/PaymentPurpose.java
+++
@@ -1,6 +0,0 @@
-package store._0982.point.domain.constant;
-
-public enum PaymentPurpose {
-    CHARGE, // 포인트 충전
-    ORDER   // 상품 직접 구매
-}
--- point/src/main/java/store/_0982/point/domain/constant/PointCategory.java
+++
@@ -1,6 +0,0 @@
-package store._0982.point.domain.constant;
-
-public enum PointCategory {
-    PAID,   // 유상 포인트 (직접 충전)
-    BONUS   // 무상 포인트 (이벤트, 적립 등)
-}
