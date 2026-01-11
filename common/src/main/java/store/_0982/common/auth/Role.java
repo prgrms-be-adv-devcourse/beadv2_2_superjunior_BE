b780907442479dcce1c08d523e20bbc92ad12ba3
AidenY915 <aiden915y@gmail.com>
2026-01-11 17:53:33 +0900

    feat:common Role에 GUEST 추가

--- common/src/main/java/store/_0982/common/auth/Role.java
+++ common/src/main/java/store/_0982/common/auth/Role.java
@@ -1,7 +1,9 @@
 package store._0982.common.auth;
 
 public enum Role {
+    GUEST,      // 미로그인 사용자
     CONSUMER,    // 구매자
     SELLER,      // 판매자
-    ADMIN        // 관리자
+    ADMIN,        // 관리자
+    ;
 }
