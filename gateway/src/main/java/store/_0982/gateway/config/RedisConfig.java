39048b7ba142b102f0fec5a5bb4f04be74ea3f51
AidenY915 <aiden915y@gmail.com>
2026-01-11 17:49:13 +0900

    feat:gateway 멤버 역할 조회용 redis 구현 및 설정

--- gateway/src/main/java/store/_0982/gateway/config/RedisConfig.java
+++ gateway/src/main/java/store/_0982/gateway/config/RedisConfig.java
@@ -1,34 +1,25 @@
 package store._0982.gateway.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import store._0982.gateway.domain.Member;
 
 @Configuration
 public class RedisConfig {
-
+    @Primary
     @Bean
-    public ReactiveRedisTemplate<String, Member> memberReactiveRedisTemplate(
+    public ReactiveRedisTemplate<String, String> memberReactiveRedisTemplate(
             ReactiveRedisConnectionFactory connectionFactory
     ) {
         StringRedisSerializer keySerializer = new StringRedisSerializer();
+        StringRedisSerializer valueSerializer = new StringRedisSerializer();
 
-        ObjectMapper objectMapper = new ObjectMapper()
-                .findAndRegisterModules() // JavaTimeModule 등 자동 등록
-                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
-        Jackson2JsonRedisSerializer<Member> valueSerializer =
-                new Jackson2JsonRedisSerializer<>(objectMapper, Member.class);
-
-        RedisSerializationContext<String, Member> context =
-                RedisSerializationContext.<String, Member>newSerializationContext(keySerializer)
+        RedisSerializationContext<String, String> context =
+                RedisSerializationContext.<String, String>newSerializationContext(keySerializer)
                         .value(valueSerializer)
                         .hashKey(keySerializer)
                         .hashValue(valueSerializer)
--- gateway/src/main/java/store/_0982/gateway/infrastructure/MemberRedisCache.java
+++ gateway/src/main/java/store/_0982/gateway/infrastructure/MemberRedisCache.java
@@ -6,22 +6,32 @@ import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 import store._0982.gateway.domain.Member;
 import store._0982.gateway.domain.MemberCache;
+import store._0982.gateway.domain.Role;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
 public class MemberRedisCache implements MemberCache {
 
-    private final ReactiveRedisTemplate<String, Member> redisTemplate;
+    private final ReactiveRedisTemplate<String, String> redisTemplate;
+    private static final String KEY_PREFIX = "member:role:cache:";
 
     @Override
-    public Mono<Member> findById(UUID id) {
-         return redisTemplate.opsForValue().get(buildKey(id));
+    public Mono<Member> findById(UUID memberId) {
+        return redisTemplate.opsForValue()
+                .get(generateKey(memberId))
+                .flatMap(roleString -> {
+                    try {
+                        Role role = Role.valueOf(roleString);
+                        return Mono.just(Member.of(memberId, role));
+                    } catch (IllegalArgumentException e) {
+                        return Mono.empty();
+                    }
+                });
     }
 
-    private String buildKey(UUID id) {
-        return "member:" + id;
+    private String generateKey(UUID memberId) {
+        return KEY_PREFIX + memberId;
     }
 }
