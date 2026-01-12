7ed02aeb3d10532b11cbad1aad1c876f1cbaff0d
Huiyeongkim <huiyeong9619@naver.com>
2026-01-12 11:16:30 +0900

    feat: sellerbalance 생성하는 내부 API

--- commerce/src/main/java/store/_0982/commerce/application/sellerbalance/SellerBalanceService.java
+++ commerce/src/main/java/store/_0982/commerce/application/sellerbalance/SellerBalanceService.java
@@ -5,6 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import store._0982.commerce.application.sellerbalance.dto.SellerBalanceCommand;
+import store._0982.commerce.application.sellerbalance.dto.SellerBalanceThumbnailInfo;
+import store._0982.commerce.exception.CustomErrorCode;
 import store._0982.common.dto.PageResponse;
 import store._0982.commerce.application.sellerbalance.dto.SellerBalanceHistoryInfo;
 import store._0982.commerce.application.sellerbalance.dto.SellerBalanceInfo;
@@ -12,6 +15,7 @@ import store._0982.commerce.domain.sellerbalance.SellerBalance;
 import store._0982.commerce.domain.sellerbalance.SellerBalanceHistory;
 import store._0982.commerce.domain.sellerbalance.SellerBalanceHistoryRepository;
 import store._0982.commerce.domain.sellerbalance.SellerBalanceRepository;
+import store._0982.common.exception.CustomException;
 
 import java.util.UUID;
 
@@ -23,9 +27,15 @@ public class SellerBalanceService {
     private final SellerBalanceRepository sellerBalanceRepository;
     private final SellerBalanceHistoryRepository sellerBalanceHistoryRepository;
 
+    public SellerBalanceThumbnailInfo createSellerBalance(SellerBalanceCommand command) {
+        SellerBalance sellerBalance = new SellerBalance(command.sellerId());
+        sellerBalanceRepository.save(sellerBalance);
+        return SellerBalanceThumbnailInfo.from(sellerBalance);
+    }
+
     public SellerBalanceInfo getBalance(UUID memberId) {
         SellerBalance findSellerBalance = sellerBalanceRepository.findByMemberId(memberId)
-                .orElse(new SellerBalance(memberId));
+                .orElseThrow(() -> new CustomException(CustomErrorCode.SELLER_NOT_FOUND));
 
         return SellerBalanceInfo.from(findSellerBalance);
     }
