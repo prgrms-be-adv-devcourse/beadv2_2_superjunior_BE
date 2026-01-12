3e5d039af621a1cdac86fe902dc898a3748ecfb3
AidenY915 <aiden915y@gmail.com>
2026-01-12 12:39:29 +0900

    refactor: gateway 인가에서 역할 조회 제거

--- gateway/src/main/java/store/_0982/gateway/config/RouteAuthorizationManager.java
+++ gateway/src/main/java/store/_0982/gateway/config/RouteAuthorizationManager.java
@@ -9,26 +9,21 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 import store._0982.gateway.domain.GatewayRouteRepository;
-import store._0982.gateway.domain.Member;
-import store._0982.gateway.domain.MemberCache;
 
-import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
 public class RouteAuthorizationManager implements ReactiveAuthorizationManager<AuthorizationContext> {
 
     private final GatewayRouteRepository gatewayRouteRepository;
-    private final MemberCache memberCache;
 
     @Override
     public Mono<AuthorizationDecision> check(Mono<Authentication> authentication, AuthorizationContext context) {
         ServerWebExchange exchange = context.getExchange();
         return authentication
-                .map(Authentication::getPrincipal)
-                .ofType(UUID.class)
-                .flatMap(this::getRole)
-                .flatMap(member -> {
+                .ofType(MemberAuthenticationToken.class)
+                .flatMap(memberAuthenticationToken -> {
+                    String memberRole = memberAuthenticationToken.getRole().name();
                     String method = exchange.getRequest().getMethod().name();
                     String path = exchange.getRequest().getURI().getPath();
                     return gatewayRouteRepository
@@ -37,15 +32,10 @@ public class RouteAuthorizationManager implements ReactiveAuthorizationManager<A
                                 if (gatewayRoute == null || gatewayRoute.getRoles() == null) {
                                     return new AuthorizationDecision(false);
                                 }
-                                boolean allowed = gatewayRoute.getRoles().contains(member.getRole().name());
+                                boolean allowed = gatewayRoute.getRoles().contains(memberRole);
                                 return new AuthorizationDecision(allowed);
                             })
                             .defaultIfEmpty(new AuthorizationDecision(false));
                 });
-    }
-
-    private Mono<Member> getRole(UUID memberId){
-        if (memberId == UUID.fromString("00000000-0000-0000-0000-000000000000")) return Mono.just(Member.createGuest());
-        return memberCache.findById(memberId);
     }
 }
