6104f7742e5f223b5b4e32d58bbcf351a8ff3511
Huiyeongkim <huiyeong9619@naver.com>
2026-01-15 10:27:04 +0900

    refactor: settlement schema 생성

--- batch/src/main/java/store/_0982/batch/domain/settlement/Settlement.java
+++ batch/src/main/java/store/_0982/batch/domain/settlement/Settlement.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name = "settlement", schema = "order_schema")
+@Table(name = "settlement", schema = "settlement_schema")
 public class Settlement {
 
     @Id
