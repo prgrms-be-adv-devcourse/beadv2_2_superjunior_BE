330acfd1050d0ead9a573e8f1178a39cb9019b11
AidenY915 <aiden915y@gmail.com>
2026-01-15 16:47:53 +0900

    fix: kafkaConfig 충돌 해결

--- member/src/main/java/store/_0982/member/config/member/MemberKafkaConfig.java
+++ member/src/main/java/store/_0982/member/config/member/MemberKafkaConfig.java
@@ -6,16 +6,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.ProducerFactory;
 import store._0982.common.kafka.KafkaCommonConfigs;
-import store._0982.common.kafka.dto.BaseEvent;
+import store._0982.common.kafka.dto.MemberDeletedEvent;
 
 @EnableKafka
 @Configuration
-public class KafkaConfig {
+public class MemberKafkaConfig {
     @Value("${kafka.bootstrap-servers}")
     private String bootStrapServers;
 
     @Bean
-    public ProducerFactory<String, BaseEvent> memberDeletedProducerFactory() {
+    public ProducerFactory<String, MemberDeletedEvent> memberDeletedProducerFactory() {
         return KafkaCommonConfigs.defaultProducerFactory(bootStrapServers);
     }
 }
