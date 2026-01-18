bd745a5984cc3e4e7259e824a1f6de2d941928f0
AidenY915 <aiden915y@gmail.com>
2026-01-18 20:07:51 +0900

    feat: PersonalVector 계산 utill 클래스 틀

---
+++ batch/src/main/java/store/_0982/batch/domain/ai/VectorUtil.java
@@ -0,0 +1,10 @@
+package store._0982.batch.domain.ai;
+
+import java.util.List;
+
+public class VectorUtil {
+
+    public static float[] getAverageVector(List<CartVector> cartVectors, List<OrderVector> orderVectors) {
+        return null;
+    }
+}
