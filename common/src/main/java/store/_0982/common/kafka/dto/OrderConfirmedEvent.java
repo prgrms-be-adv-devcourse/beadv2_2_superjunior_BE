fec2da22542e2f9eab2a7699809eafd992663781
minbros <pristo24631@gmail.com>
2026-01-19 15:02:24 +0900

    feat: OrderConfirmedEvent에 필드 추가

--- common/src/main/java/store/_0982/common/kafka/dto/OrderConfirmedEvent.java
+++ common/src/main/java/store/_0982/common/kafka/dto/OrderConfirmedEvent.java
@@ -15,12 +15,26 @@ public class OrderConfirmedEvent extends BaseEvent {
 
     private UUID orderId;
     private UUID memberId;
+    private UUID groupPurchaseId;
     private String productName;
+    private ProductCategory productCategory;
 
     public OrderConfirmedEvent(Clock clock, UUID orderId, UUID memberId, String productName) {
         super(clock);
         this.orderId = orderId;
         this.memberId = memberId;
         this.productName = productName;
+    }
+
+    public enum ProductCategory {
+        HOME,        // 생활 & 주방
+        FOOD ,       // 식품 & 간식
+        HEALTH,      // 건강 & 헬스
+        BEAUTY,      // 뷰티
+        FASHION,     // 패션 & 의류
+        ELECTRONICS, // 전자 & 디지털
+        KIDS,        // 유아 & 어린이
+        HOBBY,       // 취미
+        PET          // 반려동물
     }
 }
