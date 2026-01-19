ed09070e798069ba29e8f79db03fff5a7b6f8784
minbros <pristo24631@gmail.com>
2026-01-19 20:55:17 +0900

    fix: OrderConfirmedEvent의 테스트용 생성자에 누락된 부분 수정

--- common/src/main/java/store/_0982/common/kafka/dto/OrderConfirmedEvent.java
+++ common/src/main/java/store/_0982/common/kafka/dto/OrderConfirmedEvent.java
@@ -19,16 +19,19 @@ public class OrderConfirmedEvent extends BaseEvent {
     private String productName;
     private ProductCategory productCategory;
 
-    public OrderConfirmedEvent(Clock clock, UUID orderId, UUID memberId, String productName) {
+    public OrderConfirmedEvent(Clock clock, UUID orderId, UUID memberId, UUID groupPurchaseId,
+                               String productName, ProductCategory productCategory) {
         super(clock);
         this.orderId = orderId;
         this.memberId = memberId;
+        this.groupPurchaseId = groupPurchaseId;
         this.productName = productName;
+        this.productCategory = productCategory;
     }
 
     public enum ProductCategory {
         HOME,        // 생활 & 주방
-        FOOD ,       // 식품 & 간식
+        FOOD,        // 식품 & 간식
         HEALTH,      // 건강 & 헬스
         BEAUTY,      // 뷰티
         FASHION,     // 패션 & 의류
