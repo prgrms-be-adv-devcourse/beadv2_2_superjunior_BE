7ef42310f6b4836412814ae12ad833cf7fc86303
AidenY915 <aiden915y@gmail.com>
2026-01-19 10:17:08 +0900

    feat: batch-service VectorUtil 초안

--- batch/src/main/java/store/_0982/batch/domain/ai/ProductVector.java
+++ batch/src/main/java/store/_0982/batch/domain/ai/ProductVector.java
@@ -1,10 +1,12 @@
 package store._0982.batch.domain.ai;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.UUID;
 
 @AllArgsConstructor
+@Getter
 public abstract class ProductVector {
     private UUID memberId;
     private UUID productId;
