d62ad3b6ebe376f1d4ce26d077110a0724be64ea
Huiyeongkim <huiyeong9619@naver.com>
2026-01-19 14:37:59 +0900

    refactor: 주문 취소 OrderCommandService로 변경

--- commerce/src/main/java/store/_0982/commerce/application/order/OrderService.java
+++ commerce/src/main/java/store/_0982/commerce/application/order/OrderService.java
@@ -2,26 +2,14 @@ package store._0982.commerce.application.order;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import store._0982.commerce.application.grouppurchase.GroupPurchaseService;
 import store._0982.commerce.application.order.dto.*;
-import store._0982.commerce.application.order.event.OrderCancelProcessedEvent;
-import store._0982.commerce.application.sellerbalance.SellerBalanceService;
-import store._0982.commerce.domain.grouppurchase.GroupPurchase;
 import store._0982.commerce.domain.order.Order;
-import store._0982.commerce.domain.order.OrderRepository;
-import store._0982.commerce.domain.order.OrderStatus;
-import store._0982.commerce.exception.CustomErrorCode;
 import store._0982.common.dto.PageResponse;
-import store._0982.common.exception.CustomException;
 
 import java.util.List;
 import java.util.UUID;
-
-import static store._0982.commerce.domain.order.OrderCancellationPolicy.*;
 
 @Slf4j
 @Service
@@ -30,13 +18,6 @@ public class OrderService {
 
     private final OrderCommandService orderCommandService;
     private final OrderQueryService orderQueryService;
-    private final OrderRepository orderRepository;
-
-    private final GroupPurchaseService groupPurchaseService;
-    private final SellerBalanceService sellerBalanceService;
-
-    private final ApplicationEventPublisher eventPublisher;
-
 
     /**
      * 주문 생성
@@ -93,72 +74,21 @@ public class OrderService {
         return orderQueryService.getOrdersByConsumer(memberId, pageable);
     }
 
-    @Transactional
+    /**
+     * 주문 취소
+     *
+     * @param command
+     */
     public void cancelOrder(OrderCancelCommand command) {
-        Order order = orderRepository.findById(command.orderId())
-                .orElseThrow(() -> new CustomException(CustomErrorCode.ORDER_NOT_FOUND));
-
-        GroupPurchase groupPurchase = groupPurchaseService
-                .findByGroupPurchase(order.getGroupPurchaseId());
-
-        if (order.getStatus() == OrderStatus.PAYMENT_COMPLETED) {
-            processCancellationBeforeSuccess(order, groupPurchase, command.reason());
-            return;
-        }
-
-        if (groupPurchase.isInReversedPeriod()) {
-            processCancellationWithin48Hours(order, groupPurchase.getSellerId(), command.reason());
-            return;
-        }
-
-        if (groupPurchase.isInReturnedPeriod()) {
-            processReturnAfter48Hours(order, groupPurchase.getSellerId(), command.reason());
-            return;
-        }
-        throw new CustomException(CustomErrorCode.ORDER_CANCELLATION_NOT_ALLOWED);
+        orderCommandService.cancelOrder(command);
     }
 
-    private void processCancellationBeforeSuccess(Order order, GroupPurchase groupPurchase, String reason) {
-        groupPurchaseService.cancelOrder(groupPurchase.getGroupPurchaseId(), order.getQuantity());
-
-        order.requestCancel();
-
-        RefundAmount refundAmount = calculate(order, CancellationType.BEFORE_GROUP_PURCHASE_SUCCESS);
-        publishCancellationEvent(order, reason, refundAmount.refundAmount());
-    }
-
-    private void processCancellationWithin48Hours(Order order, UUID sellerId, String reason) {
-        order.requestReversed();
-
-        RefundAmount refundAmount = calculate(order, CancellationType.WITHIN_48_HOURS);
-
-        // 판매자에게 수수료 지급
-        if (refundAmount.cancellationFee() > 0) {
-            sellerBalanceService.addFee(sellerId, refundAmount.cancellationFee());
-        }
-
-        publishCancellationEvent(order, reason, refundAmount.refundAmount());
-    }
-
-    private void processReturnAfter48Hours(Order order, UUID sellerId, String reason) {
-        order.requestReturned();
-
-        RefundAmount refundAmount = calculate(order, CancellationType.AFTER_48_HOURS);
-
-        // 판매자에게 수수료 지급
-        if (refundAmount.cancellationFee() > 0) {
-            sellerBalanceService.addFee(sellerId, refundAmount.cancellationFee());
-        }
-
-        publishCancellationEvent(order, reason, refundAmount.refundAmount());
-    }
-
-    private void publishCancellationEvent(Order order, String reason, Long refundAmount) {
-        eventPublisher.publishEvent(
-                new OrderCancelProcessedEvent(order, reason, refundAmount)
-        );
-    }
-
+    /**
+     * 모든 주문 조회
+     *
+     * @param memberId
+     * @return
+     */
     public List<Order> getAllOrderByMemberId(UUID memberId) {
         return orderQueryService.getAllOrderByMemberId(memberId);
     }
