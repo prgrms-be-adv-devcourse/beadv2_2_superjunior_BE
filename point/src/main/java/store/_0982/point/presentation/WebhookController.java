fa03330729e6f4df5ca1b6929ba26f37d0abfe05
minbros <pristo24631@gmail.com>
2026-01-20 12:09:43 +0900

    WIP

---
+++ point/src/main/java/store/_0982/point/presentation/WebhookController.java
@@ -0,0 +1,14 @@
+package store._0982.point.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/webhooks")
+public class WebhookController {
+
+
+
+}
