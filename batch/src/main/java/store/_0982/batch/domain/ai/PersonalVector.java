076753df4ad8f40a909403c0bbe9cbf29fc0743f
AidenY915 <aiden915y@gmail.com>
2026-01-20 17:39:20 +0900

    feat: batch-service PersonalVector 필드명 수정

--- batch/src/main/java/store/_0982/batch/domain/ai/PersonalVector.java
+++ batch/src/main/java/store/_0982/batch/domain/ai/PersonalVector.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.type.SqlTypes;
@@ -18,15 +17,16 @@ import java.util.UUID;
 @NoArgsConstructor
 public class PersonalVector {
     @Id
+    @Column(name = "member_id")
     private UUID memberId;
 
     @Column(name = "vector", nullable = false, columnDefinition = "vector(1536)")
     @JdbcTypeCode(SqlTypes.VECTOR)
     private float[] vector;
 
+    @Column(name = "updated_at")
     @UpdateTimestamp
-    @CreationTimestamp
-    private OffsetDateTime updateTime;
+    private OffsetDateTime updateAt;
 
     private PersonalVector(UUID memberId, float[] vector) {
         this.memberId = memberId;
