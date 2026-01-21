a2c88979b33c6fc70917b9dbcaa292289cbc1a0d
ajw <anjy0821@naver.com>
2026-01-21 15:07:45 +0900

    Fix: event publish 구조 수정

--- commerce/src/main/java/store/_0982/commerce/infrastructure/event/listener/ProductEventListener.java
+++ commerce/src/main/java/store/_0982/commerce/infrastructure/event/listener/ProductEventListener.java
@@ -2,27 +2,20 @@ package store._0982.commerce.infrastructure.event.listener;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 import store._0982.commerce.application.product.event.ProductUpsertedEvent;
-import store._0982.common.kafka.KafkaTopics;
+import store._0982.commerce.infrastructure.kafka.publisher.ProductKafkaEventPublisher;
 
 @Component
 @RequiredArgsConstructor
 public class ProductEventListener {
 
-    private final KafkaTemplate<String, store._0982.common.kafka.dto.ProductUpsertedEvent> kafkaTemplate;
+    private final ProductKafkaEventPublisher eventPublisher;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleUpserted(ProductUpsertedEvent event) {
-        store._0982.common.kafka.dto.ProductUpsertedEvent kafkaEvent = event.product().toEvent(event.product().getCategory());
-
-        kafkaTemplate.send(
-                KafkaTopics.PRODUCT_UPSERTED,
-                kafkaEvent.getProductId().toString(),
-                kafkaEvent
-        );
+        eventPublisher.pulbish(event);
     }
 }
