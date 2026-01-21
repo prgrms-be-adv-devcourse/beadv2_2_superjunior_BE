991b54c00ea4984ccae3f48f20187bf3c8f79577
Huiyeongkim <huiyeong9619@naver.com>
2026-01-21 10:17:38 +0900

    refactor: commerce entity와 동일하도록 batch entity 수정

--- batch/src/main/java/store/_0982/batch/domain/grouppurchase/GroupPurchase.java
+++ batch/src/main/java/store/_0982/batch/domain/grouppurchase/GroupPurchase.java
@@ -19,6 +19,10 @@ public class GroupPurchase {
     @Id
     private UUID groupPurchaseId;
 
+    @Version
+    @Column(name = "version")
+    private Long version;
+
     @Column(name = "min_quantity", nullable = false)
     private int minQuantity;
 
@@ -53,6 +57,15 @@ public class GroupPurchase {
     @Column(name = "current_quantity", nullable = false)
     private int currentQuantity = 0;
 
+    @Column(name = "returned_at")
+    private OffsetDateTime returnedAt;
+
+    @Column(name = "succeeded_at")
+    private OffsetDateTime succeededAt;
+
+    @Column(name = "settled_at")
+    private OffsetDateTime settledAt; // 정산 완료 시간
+
     @Column(name = "created_at", nullable = false)
     @CreationTimestamp
     private OffsetDateTime createdAt;
@@ -60,12 +73,6 @@ public class GroupPurchase {
     @Column(name = "updated_at")
     @UpdateTimestamp
     private OffsetDateTime updatedAt;
-
-    @Column(name = "returned_at")
-    private OffsetDateTime returnedAt;
-
-    @Column(name = "succeeded_at")
-    private OffsetDateTime succeededAt;
 
     public GroupPurchase(int mintQuantity,
                          int maxQuantity,
