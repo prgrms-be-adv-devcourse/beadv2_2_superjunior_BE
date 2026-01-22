24dd170063bce46bbf6628c338907dc217e95b29
Huiyeongkim <huiyeong9619@naver.com>
2026-01-22 14:59:33 +0900

    fix: db schema error 수정

--- batch/src/main/resources/db/migration/V3__add_order_settlement.sql
+++ batch/src/main/resources/db/migration/V3__add_order_settlement.sql
@@ -7,7 +7,7 @@ CREATE TABLE settlement_schema.order_settlement
     total_amount        BIGINT                      NOT NULL,
     order_status        VARCHAR(255)                NOT NULL,
     settlement_id       UUID,
-    created_at          TIMESTAMP WITH TIME ZONE NOT NULL,
-    settled_id          TIMESTAMP WITH TIME ZONE,
+    created_at          TIMESTAMP WITH    TIME ZONE NOT NULL,
+    settled_id          TIMESTAMP WITH    TIME ZONE,
     CONSTRAINT pk_order_settlement PRIMARY KEY (order_settlement_id)
 );
