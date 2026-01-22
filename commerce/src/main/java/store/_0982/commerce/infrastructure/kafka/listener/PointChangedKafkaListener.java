2e597ed3300e25bf10d9636edea9ff8346a9cef1
sumyeom <wjstndus75@naver.com>
2026-01-22 15:10:59 +0900

    feat: Point Update 이벤트 전달 시 처리 추가

---
+++ commerce/src/main/java/store/_0982/commerce/infrastructure/kafka/listener/PointChangedKafkaListener.java
@@ -0,0 +1,35 @@
+package store._0982.commerce.infrastructure.kafka.listener;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import store._0982.commerce.application.order.OrderPaymentProcessorService;
+import store._0982.common.exception.CustomException;
+import store._0982.common.kafka.KafkaTopics;
+import store._0982.common.kafka.dto.PointChangedEvent;
+import store._0982.common.log.ServiceLog;
+
+@RequiredArgsConstructor
+@Service
+public class PointChangedKafkaListener {
+
+    private final OrderPaymentProcessorService orderPaymentProcessorService;
+
+    @ServiceLog
+    @Transactional
+    @RetryableTopic(
+            kafkaTemplate = "retryKafkaTemplate",
+            exclude = CustomException.class
+    )
+    @KafkaListener(
+            topics = KafkaTopics.POINT_CHANGED,
+            groupId = "order-service-group",
+            containerFactory = "paymentKafkaListenerFactory"
+    )
+    public void handlePointChangedEvent(PointChangedEvent event) {
+        orderPaymentProcessorService.processPointStatusUpdate(event);
+    }
+
+}
