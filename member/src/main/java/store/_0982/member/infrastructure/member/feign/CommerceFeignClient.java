88d3e9e07be91faea5ef0ba1533b852519421f3e
AidenY915 <aiden915y@gmail.com>
2026-01-22 09:34:00 +0900

    fix: member-service CommerceFeign Dto 변경

--- member/src/main/java/store/_0982/member/infrastructure/member/feign/CommerceFeignClient.java
+++ member/src/main/java/store/_0982/member/infrastructure/member/feign/CommerceFeignClient.java
@@ -1,18 +1,16 @@
 package store._0982.member.infrastructure.member.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import store._0982.common.dto.ResponseDto;
-
-import java.util.UUID;
+import store._0982.member.infrastructure.member.feign.dto.SellerBalanceRequest;
 
 @FeignClient(
         name = "commerce-service",
         url = "localhost:8087"
 )
 public interface CommerceFeignClient {
-    @PostMapping(value = "/internal/balances", consumes = MediaType.APPLICATION_JSON_VALUE)
-    ResponseDto<Void> postSellerBalance(@RequestBody UUID sellerId);
+    @PostMapping(value = "/internal/balances")
+    ResponseDto<Void> postSellerBalance(@RequestBody SellerBalanceRequest sellerBalanceRequest);
 }
---
+++ member/src/main/java/store/_0982/member/infrastructure/member/feign/dto/SellerBalanceRequest.java
@@ -0,0 +1,8 @@
+package store._0982.member.infrastructure.member.feign.dto;
+
+import java.util.UUID;
+
+public record SellerBalanceRequest(
+        UUID sellerId
+) {
+}
