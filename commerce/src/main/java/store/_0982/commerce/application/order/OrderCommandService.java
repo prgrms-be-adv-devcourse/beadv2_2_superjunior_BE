5717935a910590457e4ed3e2baf6ec37e90110d2
Huiyeongkim <huiyeong9619@naver.com>
2026-01-23 16:45:52 +0900

    refactor: OrderPaymentProcessorService에서 수행되도록 변경

--- commerce/src/main/java/store/_0982/commerce/application/order/OrderCommandService.java
+++ commerce/src/main/java/store/_0982/commerce/application/order/OrderCommandService.java
@@ -20,8 +20,6 @@ import store._0982.commerce.application.order.event.OrderCancelProcessedEvent;
 import store._0982.commerce.application.order.event.OrderCartCompletedEvent;
 import store._0982.commerce.application.order.event.OrderCreateProcessedEvent;
 import store._0982.commerce.application.product.ProductService;
-import store._0982.commerce.application.sellerbalance.SellerBalanceService;
-import store._0982.commerce.application.settlement.OrderSettlementService;
 import store._0982.commerce.domain.cart.Cart;
 import store._0982.commerce.domain.grouppurchase.GroupPurchase;
 import store._0982.commerce.domain.order.Order;
@@ -54,12 +52,11 @@ public class OrderCommandService {
     private final ProductService productService;
     private final GroupPurchaseService groupPurchaseService;
     private final ParticipateService participateService;
-    private final SellerBalanceService sellerBalanceService;
 
     private final MemberClient memberClient;
 
     private final ApplicationEventPublisher eventPublisher;
-    private final OrderSettlementService orderSettlementService;
+
 
     @ServiceLog
     @Transactional
@@ -308,12 +305,6 @@ public class OrderCommandService {
                 orderRepository.bulkMarkGroupPurchaseSuccess(event.getId());
             }
         }
-    }
-
-    @Transactional
-    public void completeCancellation(Order order) {
-        order.changeStatus();
-        orderSettlementService.saveOrder(order);
     }
 
     @Transactional
