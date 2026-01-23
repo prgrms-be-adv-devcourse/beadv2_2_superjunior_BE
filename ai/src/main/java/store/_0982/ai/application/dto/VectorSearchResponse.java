64696ec7e6714e60b72e83538564684f334e5649
ajw <anjy0821@naver.com>
2026-01-23 09:20:11 +0900

    Fix: GroupPurchase id타입을 uuid로 수정

--- ai/src/main/java/store/_0982/ai/application/dto/VectorSearchResponse.java
+++ ai/src/main/java/store/_0982/ai/application/dto/VectorSearchResponse.java
@@ -4,9 +4,10 @@ import store._0982.ai.infrastructure.feign.search.dto.ProductSearchInfo;
 import store._0982.ai.infrastructure.feign.search.dto.ProductVectorInfo;
 
 import java.time.OffsetDateTime;
+import java.util.UUID;
 
 public record VectorSearchResponse(
-        String groupPurchaseId,
+        UUID groupPurchaseId,
         Integer minQuantity,
         Integer maxQuantity,
         String title,
