2ed0766e10579b3b628c9b2b6f6b4469dba0e075
minbros <pristo24631@gmail.com>
2026-01-25 10:33:53 +0900

    fix: 웹훅 엔드포인트 변경

--- point/src/main/java/store/_0982/point/presentation/WebhookController.java
+++ point/src/main/java/store/_0982/point/presentation/WebhookController.java
@@ -14,7 +14,7 @@ import java.time.OffsetDateTime;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/webhooks")
+@RequestMapping("/webhooks")
 public class WebhookController {
 
     private final TossWebhookFacade tossWebhookFacade;
