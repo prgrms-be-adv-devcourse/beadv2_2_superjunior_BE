8724a47499ed3c84802de642f7a7193ad8391e07
AidenY915 <aiden915y@gmail.com>
2026-02-02 17:03:20 +0900

    feat:dummy-data 프로젝트 추가

---
+++ dummy-data/src/main/java/store_0982/dummy_data/DummyDataApplication.java
@@ -0,0 +1,13 @@
+package store_0982.dummy_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DummyDataApplication.class, args);
+    }
+
+}
