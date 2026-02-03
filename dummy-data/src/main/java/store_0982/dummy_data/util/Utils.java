6030ebe817faa5c1dc829419d8e6a7f11162e81d
ajw <anjy0821@naver.com>
2026-02-03 15:35:51 +0900

    FIx: 더미 데이터 csv 러너

--- dummy-data/src/main/java/store_0982/dummy_data/util/Utils.java
+++ dummy-data/src/main/java/store_0982/dummy_data/util/Utils.java
@@ -1,13 +1,13 @@
 package store_0982.dummy_data.util;
 
-import store._0982.member.domain.member.Member;
+
 
 import java.lang.reflect.Field;
 
 public class Utils {
-    public static void setField(Object target, String fieldName, Object value) {
+    public static <T> void setField(T target, String fieldName, Object value) {
         try {
-            Field field = Member.class.getDeclaredField(fieldName);
+            Field field = target.getClass().getDeclaredField(fieldName);
             field.setAccessible(true);
             field.set(target, value);
         } catch (NoSuchFieldException | IllegalAccessException e) {
