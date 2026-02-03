a4f0e7460b89158f132f472bcb3cb65f98890f60
Huiyeongkim <huiyeong9619@naver.com>
2026-02-03 13:33:17 +0900

    refactor: 용도에 맞게 OrderStatus 변경

--- common/src/main/java/store/_0982/common/domain/order/OrderStatus.java
+++ common/src/main/java/store/_0982/common/domain/order/OrderStatus.java
@@ -8,27 +8,12 @@ public enum OrderStatus {
     PAYMENT_COMPLETED,      // 결제 완료
     PAYMENT_FAILED,         // 결제 실패
 
-    ORDER_FAILED,           // 주문 실패
-
-    GROUP_PURCHASE_SUCCESS, // 공동 구매 성공
-    GROUP_PURCHASE_FAIL,    // 공동 구매 실패
-
-    CANCEL_REQUESTED,       // 취소 요청
-    CANCELLED,              // 취소 완료
-
-    REVERSE_REQUESTED,      // 번복 요청
-    REVERSED,               // 번복 완료
-
-    REFUND_REQUESTED,       // 반품 요청
-    REFUNDED,               // 반품 완료
-
-    PURCHASE_CONFIRMED;      // 구매 확정
+    CANCELLED,               // 주문 취소
+    CONFIRMED;               // 구매 확정
 
     public static List<OrderStatus> participantStatuses(){
         return List.of(
-                PAYMENT_COMPLETED,
-                GROUP_PURCHASE_SUCCESS,
-                GROUP_PURCHASE_FAIL
+                PAYMENT_COMPLETED
         );
     }
 }
