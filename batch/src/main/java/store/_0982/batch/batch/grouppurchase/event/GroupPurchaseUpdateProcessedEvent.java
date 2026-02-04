e8f6648046a5a8cd12703fec140ce10d3e97eaf3
sumyeom <wjstndus75@naver.com>
2026-02-04 15:58:02 +0900

    refactor: grouppurchase batch 리팩토링 wip

--- batch/src/main/java/store/_0982/batch/batch/grouppurchase/event/GroupPurchaseUpdateProcessedEvent.java
+++
@@ -1,9 +0,0 @@
-package store._0982.batch.batch.grouppurchase.event;
-
-import store._0982.batch.batch.grouppurchase.dto.GroupPurchaseResultWithProductInfo;
-
-public record GroupPurchaseUpdateProcessedEvent(
-        GroupPurchaseResultWithProductInfo updateItem
-) {
-
-}
--- batch/src/main/java/store/_0982/batch/infrastructure/messaging/kafka/GroupPurchaseFailedEventMapper.java
+++ batch/src/main/java/store/_0982/batch/infrastructure/messaging/kafka/GroupPurchaseFailedEventMapper.java
@@ -1,14 +1,14 @@
 package store._0982.batch.infrastructure.messaging.kafka;
 
-import store._0982.common.domain.grouppurchase.GroupPurchase;
+import store._0982.batch.batch.grouppurchase.dto.GroupPurchaseResultProjection;
 import store._0982.common.kafka.dto.GroupPurchaseFailedEvent;
 
 public class GroupPurchaseFailedEventMapper {
 
-    public static GroupPurchaseFailedEvent toMessage(GroupPurchase groupPurchase, String reason){
+    public static GroupPurchaseFailedEvent toMessage(GroupPurchaseResultProjection item) {
         return new GroupPurchaseFailedEvent(
-                groupPurchase.getGroupPurchaseId(),
-                reason
+                item.groupPurchaseId(),
+                "최소 수량 미달"
         );
     }
 }
