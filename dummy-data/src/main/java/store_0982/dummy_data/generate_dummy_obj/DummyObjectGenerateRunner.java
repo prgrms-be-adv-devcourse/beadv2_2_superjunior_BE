9132bcf05d3da5012f95c673dd75cf791a528d7d
ajw <anjy0821@naver.com>
2026-02-05 15:13:46 +0900

    Feat: order 더미데이터 생성

--- dummy-data/src/main/java/store_0982/dummy_data/generate_dummy_obj/DummyObjectGenerateRunner.java
+++ dummy-data/src/main/java/store_0982/dummy_data/generate_dummy_obj/DummyObjectGenerateRunner.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 import store_0982.dummy_data.generate_dummy_obj.member.DummyMemberGenerator;
 import store_0982.dummy_data.generate_dummy_obj.member.DummySellerGenerator;
 import store_0982.dummy_data.generate_dummy_obj.commerce.DummyGroupPurchaseGenerator;
+import store_0982.dummy_data.generate_dummy_obj.commerce.DummyOrderGenerator;
 import store_0982.dummy_data.generate_dummy_obj.commerce.DummyProductGenerator;
 
 @Component
@@ -22,16 +23,20 @@ public class DummyObjectGenerateRunner implements ApplicationRunner {
     private final DummyMemberGenerator dummyMemberGenerator;
     private final DummySellerGenerator dummySellerGenerator;
     private final DummyGroupPurchaseGenerator dummyGroupPurchaseGenerator;
+    private final DummyOrderGenerator dummyOrderGenerator;
 
     @Value("${dummy-data.product-id-pool.count}")
     private int productCount;
     @Value("${dummy-data.group-purchase-id-pool.count}")
     private int groupPurchaseCount;
+    @Value("${dummy-data.order-id-pool.count}")
+    private int orderCount;
     @Override
     public void run(org.springframework.boot.ApplicationArguments args) throws Exception {
         dummyProductGenerator.generateAndWriteCsv(productCount);
         dummyGroupPurchaseGenerator.generateAndWriteCsv(groupPurchaseCount);
         dummyMemberGenerator.readIdAndWriteMember();
         dummySellerGenerator.readIdAndWriteSeller();
+        dummyOrderGenerator.generateAndWriteCsv(orderCount);
     }
 }
