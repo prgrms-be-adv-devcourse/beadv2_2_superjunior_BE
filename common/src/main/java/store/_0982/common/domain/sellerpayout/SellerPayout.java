69e1eb5f17e56343b872e41da6fd7d629914565b
Huiyeongkim <huiyeong9619@naver.com>
2026-02-06 12:04:40 +0900

    refactor: 용도에 맞게 패키지 분리

--- common/src/main/java/store/_0982/common/domain/sellerpayout/SellerPayout.java
+++ common/src/main/java/store/_0982/common/domain/sellerpayout/SellerPayout.java
@@ -1,4 +1,4 @@
-package store._0982.common.domain.settlement;
+package store._0982.common.domain.sellerpayout;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
--- common/src/main/java/store/_0982/common/domain/sellerpayout/SellerPayoutStatus.java
+++ common/src/main/java/store/_0982/common/domain/sellerpayout/SellerPayoutStatus.java
@@ -1,4 +1,4 @@
-package store._0982.common.domain.settlement;
+package store._0982.common.domain.sellerpayout;
 
 public enum SellerPayoutStatus {
     PENDING,
