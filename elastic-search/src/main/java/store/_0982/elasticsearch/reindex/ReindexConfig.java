df300a0ea89ad3bcbe91aaf0635ffcfa24ca6547
ajw <anjy0821@naver.com>
2026-02-08 12:26:05 +0900

    Refactor : es 모듈에서 reindex관련 로직 삭제

--- elastic-search/src/main/java/store/_0982/elasticsearch/reindex/ReindexConfig.java
+++
@@ -1,9 +0,0 @@
-package store._0982.elasticsearch.reindex;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(GroupPurchaseReindexProperties.class)
-public class ReindexConfig {
-}
