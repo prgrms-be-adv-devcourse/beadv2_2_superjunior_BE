6abe69f820edbfa1ef23b9f7ad7bd5d196e956b8
minbros <realminbros@gmail.com>
2026-02-08 21:24:15 +0900

    test: K6 CI/CD 테스트

--- docs/script/k6/common/summary.js
+++ docs/script/k6/common/summary.js
@@ -1,6 +1,7 @@
-// noinspection JSUnresolvedReference
+// noinspection JSUnresolvedReference, JSUrlImportUsage
 
-import {htmlReport} from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/2.4.0/dist/bundle.js';
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js'
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.1.0/index.js'
 
 /**
  * k6 테스트 결과를 HTML 리포트와 텍스트 요약으로 생성
@@ -9,9 +10,8 @@ import {htmlReport} from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/
  * @returns {Object} 파일 경로를 키로, 내용을 값으로 하는 객체
  */
 export function generateSummary(data) {
-    const timestamp = __ENV.K6_TIMESTAMP || new Date().toISOString().replace(/[:.]/g, '-');
-
     return {
-        [`/opt/k6/results/report-${timestamp}.html`]: htmlReport(data),
-    };
+        'result.html': htmlReport(data),
+        stdout: textSummary(data, { indent: ' ', enableColors: true }),
+    }
 }
