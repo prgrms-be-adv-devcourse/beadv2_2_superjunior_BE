cba5e1cd4ff572591fc281084b9144871f6a2fbc
AidenY915 <aiden915y@gmail.com>
2026-02-08 21:04:55 +0900

    fix: batch 벡터 배치 writer Transactional 추가

--- batch/src/main/java/store/_0982/batch/batch/recommendation/writer/PersonalVectorWriter.java
+++ batch/src/main/java/store/_0982/batch/batch/recommendation/writer/PersonalVectorWriter.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import store._0982.batch.domain.recommendation.PersonalVector;
 import store._0982.batch.domain.recommendation.PersonalVectorRepository;
 
@@ -17,6 +18,7 @@ public class PersonalVectorWriter implements ItemWriter<List<PersonalVector>> {
     private final PersonalVectorRepository personalVectorRepository;
 
     @Override
+    @Transactional
     public void write(Chunk<? extends List<PersonalVector>> chunk) {
         List<PersonalVector> toSave = new ArrayList<>();
         for (List<PersonalVector> batch : chunk.getItems()) {
