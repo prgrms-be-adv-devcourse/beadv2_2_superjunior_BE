bc86fd63aec206c538ac35797f7d2a2fb109fd6e
Huiyeongkim <huiyeong9619@naver.com>
2026-02-11 15:13:15 +0900

    refactor: member client 대신 jdbc로 조회

--- batch/src/main/java/store/_0982/batch/application/sellerpayout/BankTransferService.java
+++ batch/src/main/java/store/_0982/batch/application/sellerpayout/BankTransferService.java
@@ -2,7 +2,7 @@ package store._0982.batch.application.sellerpayout;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import store._0982.batch.infrastructure.client.member.dto.SellerAccountInfo;
+import store._0982.batch.batch.sellerpayout.dto.SellerAccountDto;
 import store._0982.common.log.ServiceLog;
 
 @Slf4j
@@ -10,7 +10,7 @@ import store._0982.common.log.ServiceLog;
 public class BankTransferService {
 
     @ServiceLog
-    public void transfer(SellerAccountInfo sellerAccountInfo, long amount) {
+    public void transfer(SellerAccountDto sellerAccountDto, long amount) {
         try {
             Thread.sleep(100);
         } catch (InterruptedException e) {
