79c30c2eb229231d5deadcc6cfad82e9c8f6c2fa
Huiyeongkim <huiyeong9619@naver.com>
2026-02-11 12:27:49 +0900

    refactor: sort import 추가

--- commerce/src/main/java/store/_0982/commerce/presentation/product/InternalProductController.java
+++ commerce/src/main/java/store/_0982/commerce/presentation/product/InternalProductController.java
@@ -1,9 +1,9 @@
 package store._0982.commerce.presentation.product;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
