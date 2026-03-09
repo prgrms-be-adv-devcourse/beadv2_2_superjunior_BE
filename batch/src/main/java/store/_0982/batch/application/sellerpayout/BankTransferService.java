83554bf04a2c097cfac3711f418e81e44abde2df
Huiyeongkim <huiyeong9619@naver.com>
2026-03-09 09:21:22 +0900

    feat: 송금 에러나는 경우 디스코드 웹훅 알림 전송 구현

--- batch/src/main/java/store/_0982/batch/application/sellerpayout/BankTransferService.java
+++ batch/src/main/java/store/_0982/batch/application/sellerpayout/BankTransferService.java
@@ -1,19 +1,28 @@
 package store._0982.batch.application.sellerpayout;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import store._0982.batch.application.message.DiscordMessageService;
 import store._0982.batch.batch.sellerpayout.dto.SellerAccountDto;
 import store._0982.common.log.ServiceLog;
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class BankTransferService {
+
+    private final DiscordMessageService discordMessageService;
 
     @ServiceLog
     public void transfer(SellerAccountDto sellerAccountDto, long amount) {
         try {
             Thread.sleep(100);
         } catch (InterruptedException e) {
+            discordMessageService.sendDiscordWebhookMessage(
+                    new DiscordMessageService.DiscordWebhookMessage(
+                            "[ERROR] BankTransferService transfer 에러 : " + sellerAccountDto));
+
             Thread.currentThread().interrupt();
         }
     }
