dbb078c7ace8607b8bd61cbc4881f56c99704be7
Huiyeongkim <huiyeong9619@naver.com>
2026-06-02 19:03:32 +0900

    test: SettlementJob 통합테스트 구현

--- batch/src/test/java/store/_0982/batch/BatchApplicationTests.java
+++ batch/src/test/java/store/_0982/batch/BatchApplicationTests.java
@@ -18,7 +18,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 })
 @Testcontainers
 @ActiveProfiles("test")
-class BatchApplicationTests {
+public class BatchApplicationTests {
 
     @Container
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
